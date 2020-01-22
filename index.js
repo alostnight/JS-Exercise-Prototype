44424dfc3f952c099e28d5f906d55a054bcede22
Jose Paquian <alostnight1993@outlook.com>
2020-01-21 21:01:39 -0600

    done

--- index.js
+++ index.js
@@ -39,9 +39,25 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-
+function  Person(name, age) {
+  this.name = name,
+  this.age = age,
+  this.stomach = [];
 }
+Person.prototype.eat = function(someFood) {
+  if (this.stomach.length < 10) {
+    return this.stomach.push(someFood);
+  }
+};
+Person.prototype.poop = function () {
+  return (this.stomach = []);
+  
+}
+Person.prototype.toString = function () {
+  return `${this.name}, ${this.age}`;
+}
+
+
 
 /*
   TASK 2
@@ -57,9 +73,25 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
-
+function Car(model, milesPerGallon) {
+  this.model = model;
+  this.milesPerGallon = milesPerGallon;
+  this.tank = 0;
+  this.odometer = 0;
 }
+Car.prototype.fill = function(gallons) {
+  return (this.tank += gallons);
+  
+};
+Car.prototype.drive = function(distance) {
+  if (distance / this.milesPerGallon >= this.tank) {
+    this.odometer += this.milesPerGallon * this.tank;
+    this.tank = 0;
+    return `i ran out of fuel at ${this.odometer} miles!`;
+  }
+  this.odometer += distance;
+  this.tank -= distance / this.milesPerGallon;
+};
 
 /*
   TASK 3
@@ -68,18 +100,23 @@ function Car() {
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
-
+function Baby(name, age, favoriteToy) {
+  this.name = name;
+  this.age = age;
+  this.favoriteToy = favoriteToy;
 }
-
+Baby.prototype = Object.create(Person.prototype);
+Baby.prototype.play = function() {
+  return `playing with ${this.favoriteToy}`;
+};
 /* 
   TASK 4
 
   In your own words explain the four principles for the "this" keyword below:
-  1. 
-  2. 
-  3. 
-  4. 
+  1. It turns Window object when inside a function.
+  2. This can be a function that is called.
+  3. When a constructor is used, it refers to an instance of an object.
+  4. It can be used to apply a method.
 */
 
 
